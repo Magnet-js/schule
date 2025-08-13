875fec02abc81411505a3a13183a089fcc141476
Elias <elias.friedrich1@outlook.de>
2025-08-13 12:02:36 +0200

    feat: setup api server

---
+++ backend/internal/api/routes.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
+	// API-Gruppen
+	api := r.Group("/api")
+	version := api.Group("/v1")
+
+	version.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+	/* auth.RegisterRoutes(version.Group("/auth"), db) */
+}
